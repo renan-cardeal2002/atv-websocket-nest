7c32209ca0e2b3ece6a175ca6bef19cf970be341
renan <renan01022002@hotmail.com>
2024-06-13 19:39:08 -0300

    websocker nest

---
+++ src/app.gateway.ts
@@ -0,0 +1,36 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: string): void {
+    console.log(payload);
+    this.server.emit('msgToClient', payload, client.id);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client conected ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconected ${client.id}`);
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppGateway } from './app.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [],
+  providers: [AppGateway],
+})
+export class AppModule {}
