4c8e47d3a197ac04fb88f7261b45fcdfcad5491e
renan <renan01022002@hotmail.com>
2024-06-18 08:27:31 -0300

    websocket chat

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -9,20 +9,30 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+interface Message {
+  clientId: string;
+  type: 'text' | 'file';
+  content: string;
+  filename?: string;
+}
+
 @WebSocketGateway({ cors: true })
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  private messages: Message[] = [];
+
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: string): void {
-    console.log(payload);
+  handleMessage(client: Socket, payload: Message): void {
+    this.addToHistory(client.id, payload);
     this.server.emit('msgToClient', payload, client.id);
   }
 
   afterInit(server: Server) {
+    this.server.emit('history', this.messages);
     this.logger.log('Init');
   }
 
@@ -32,5 +42,17 @@ export class AppGateway
 
   handleDisconnect(client: Socket) {
     this.logger.log(`Client disconected ${client.id}`);
+  }
+
+  addToHistory(clientId: string, payload: Message) {
+    this.messages.push({
+      clientId,
+      ...payload,
+    });
+    this.showHistory();
+  }
+
+  showHistory() {
+    this.server.emit('history', this.messages);
   }
 }
